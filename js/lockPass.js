a4f228d59666fdf05742eeed5c919475d0b348ad
jieme8 <jieme8@126.com>
2015-10-24 12:14:36 +0800

    修改ui 及 代码优化

--- js/lockPass.js
+++ js/lockPass.js
@@ -1,19 +1,24 @@
 (function(){
+    var password = "1106"; //密码设置
+
 	var div = document.createElement("div");
 	div.id = "lockPassPm";
-	div.innerHTML = '<ul id="lpUl"><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li><li>*</li><li>0</li><li>#</li></ul>';
+    var ul = document.createElement("ul");
+    ul.id = "lpUl";
 	document.body.appendChild(div);
-    var lp = document.getElementById("lockPassPm");
-    var lpUl = document.getElementById("lpUl");
-    var h = window.innerHeight;
-    lp.style.height = h + "px";
-    lpUl.style.marginTop = (((h - 360 )/2)-40) + "px";
+    div.appendChild(ul);
+    ul.innerHTML = '<li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li><li>*</li><li>0</li><li>#</li>';
+    
 
-    var passArr = [];
-    var ul = lp.getElementsByTagName("ul")[0];
-    var liArr = ul.getElementsByTagName("li");
+    //设置样式
+    var h = window.innerHeight;
+    div.style.height = h + "px";
+    ul.style.marginTop = (((h - 360 )/2)-40) + "px";
+
+
+    var passArr = []; //密码
+    //var liArr = ul.getElementsByTagName("li");
     ul.addEventListener("click",function(event){
-    	//console.log(event.srcElement.innerText);
     	passAdd(event.srcElement.innerText);
     })
 
@@ -23,29 +28,17 @@
     	console.log(passArr)
     	console.log(passArr.length)
 
-    	if(passArr.length >= 4 && passPanduan()){
-    		//div.style.display = "none";
+    	if(passArr.length >= password.length && passPanduan()){
     		document.body.removeChild(div)
     	}
     }
 
     function passPanduan(){
-
     	var p = true;
-    	var pArr = ["3","6","9","8"];
-
-    	for(i=0;i<4;i++){
-    		if(passArr[i]!=pArr[i]){
-    			p = false;
-    		}
-    	}
-
+    	var pArr = password.split("");
+    	for(i=0;i<password.length;i++){passArr[i] != pArr[i] ? p = false : "";}
     	return p;
     }
-
-
-
-
 
 })(document)
 
